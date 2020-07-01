93b139e881e06b766af86cea047872dc16f339f7
kekkarlamadhu <67690732+kekkarlamadhu@users.noreply.github.com>
2020-07-01 17:07:58 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,24 @@
+import PyPDF2
+
+pdfFileObj = open('Arihant-general_knowledge.pdf', 'rb')  
+    
+# creating a pdf reader object  
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  
+    
+# printing number of pages in pdf file  
+print(pdfReader.numPages)  
+    
+# creating a page object  
+pageObj = pdfReader.getPage(0)  
+    
+# extracting text from page  
+#print(pageObj.extractText())
+
+i=0
+while i<pdfReader.numPages:
+    pageinfo=pdfReader.getPage(i)
+    print(pageinfo.extractText())
+    i=i+1
+    
+# closing the pdf file object  
+pdfFileObj.close()  
